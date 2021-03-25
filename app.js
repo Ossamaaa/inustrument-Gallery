8ee4805e5947339defc59802ac49ebbe1f9ef2c3
ossamaaa <osskhsh@gmail.com>
2021-03-25 16:17:12 +0200

    added app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+
+ 
+var student2 = prompt(' what you want to study?','Econome ,Information-technology ,Accounting ');
+
+if(student == 'Econome'){
+    
+}else if(student == 'Information-technology'){
+
+}else if (student == 'Accounting'){
+
+}else{
+    alert('Okay welcome to our college')
+}
+
+var student = prompt('What is your FullName ? ');
+
+alert('Welcome ' + student);
+
+alert('For more informations about your choice scroll down! ')
