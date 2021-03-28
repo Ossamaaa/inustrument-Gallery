bbe1387e13758c64b21b50bac5ba5ea09d5063bf
ossamaaa <osskhsh@gmail.com>
2021-03-28 15:29:42 +0300

    added loop

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
  
 var student2 = prompt(' what you want to study?','Econome ,Information-technology ,Accounting ');
 
@@ -11,9 +11,44 @@ if(student == 'Econome'){
 }else{
     alert('Okay welcome to our college')
 }
+*/
+
+/*
+alert('For more informations about your choice scroll down! ')
+*/
 
 var student = prompt('What is your FullName ? ');
 
 alert('Welcome ' + student);
 
-alert('For more informations about your choice scroll down! ')
+var userPassword = prompt('Enter the password!');
+ var password = '123456';
+
+ while(userPassword !== password){  
+      userPassword = prompt('Please enter the correct password!')
+ }
+
+
+
+ var subject = prompt('whats your subject?!','Accounting , Econome');
+     
+ while(subject !== 'Accounting' && subject !== 'Econome'){
+ 
+    subject = prompt('please enter on of the following Econome or Accounting');
+ }
+ 
+ 
+ var sub = prompt('How many lectures or subject would you like to see?');
+ 
+ var image = ''; 
+ 
+
+ for(var i = 1 ; i <=sub; i++ ){
+     if(subject === 'Accounting'){
+         image = image + '<img src="accounting.jpg"/>';
+     }else if(subject === 'Econome'){
+         image = image + '<img src="economic.jpg"/>';
+     }
+ }
+ console.log(image);
+ document.write(image); 
